2eedff695ba1d7c5184c4b0070c97841d766bd69
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-13 12:34:38 +0200

    created const and if

---
+++ snack 5/script.js
@@ -0,0 +1,11 @@
+// Crea un array vuoto.
+//  Chiedi per 6 volte all'utente di inserire un numero,
+//  se è dispari inseriscilo nell'array.
+
+const empytArray = [' '];
+let insertEl = prompt ('type a number');
+
+if ( 2 % === 1){
+    empytArray.push(insertEl);
+    console.log(empytArray);
+}
