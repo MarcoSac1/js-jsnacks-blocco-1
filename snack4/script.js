503a79b38564dcf34e6c87643e58938049746e74
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-13 12:05:18 +0200

    crated array and prompt

---
+++ snack4/script.js
@@ -0,0 +1,5 @@
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+const enterList = ['gino', 'luca', 'franco', 'antonio'];
+
+const partyPeople =prompt('type your name');
