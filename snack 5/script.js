6e367544c8f25ec9f1473654792639102bc27d86
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-13 15:32:01 +0200

    fixed condition issue

--- snack 5/script.js
+++ snack 5/script.js
@@ -2,10 +2,13 @@
 //  Chiedi per 6 volte all'utente di inserire un numero,
 //  se è dispari inseriscilo nell'array.
 
-const empytArray = [' '];
-let insertEl = prompt ('type a number');
+const empytArray = [ ];
 
-if ( 2 % === 1){
-    empytArray.push(insertEl);
-    console.log(empytArray);
+for ( let i = 0 ; i < 6 ; i++){
+    let insertEl = Number.parseInt(prompt ('type a number'), 10);
+    if ( insertEl % 2 === 1){
+        empytArray.push(insertEl);
+        
+    }
 }
+console.log(empytArray);
