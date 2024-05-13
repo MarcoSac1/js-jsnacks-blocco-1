f8e016a41ad4cde1425c480e726f4553f9751a74
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-13 12:18:54 +0200

    fixed code issue

--- snack4/script.js
+++ snack4/script.js
@@ -1,5 +1,11 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
 
 const enterList = ['gino', 'luca', 'franco', 'antonio'];
+const partyPeople = prompt('type your name');
 
-const partyPeople =prompt('type your name');
+if( enterList.includes(partyPeople)){
+    console.log('Benvenuto');
+    
+}else {
+    console.log('TU NON PUOI ENTRAREEE!!!');
+}
