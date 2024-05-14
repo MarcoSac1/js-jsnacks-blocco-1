618b9d58c6f6c8bef16cc309f7579b2ea6338e47
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-14 12:01:59 +0200

    fixed code issue

---
+++ snack 8/script.js
@@ -0,0 +1,26 @@
+// Snack8 
+// Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l'altro di verde. Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari e in verde i numeri pari.
+
+
+const numberArray=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10,];
+// const divElement = document.querySelector('div');
+
+const divGreenEL = document.createElement('div');
+divGreenEL.classList.add('green');
+
+const divRedEL = document.createElement('div');
+divRedEL.classList.add('red');
+
+
+for (let index = 0; index < numbers.length; index++){
+    const element = array[index];
+    const newElement = document.createElement('p');
+    newElement.append(numbers[index]);
+    
+    if(numbers[index] % 2 == 0 ){
+        divGreenEL.appendChild(newElement);
+
+    }else if(numbers[index] % 2 == 1){
+        divRedEL.appendChild(newElement);
+    }
+}
